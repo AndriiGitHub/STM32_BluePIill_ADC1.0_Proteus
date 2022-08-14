6a3925a564bcf259ad484f5f2e8fbcb078a7a548
AndriiGit <neo.andreu@gmail.com>
2022-08-14 14:49:13 +0300

    LCD.h

---
+++ segment.h
@@ -0,0 +1,19 @@
+/*
+ * segment.h
+ *
+ *  Created on: Aug 11, 2022
+ *      Author: neo-a
+ */
+#include "main.h"
+#include <math.h>
+
+#ifndef INC_SEGMENT_H_
+#define INC_SEGMENT_H_
+
+
+void SetNumber (double NUM);
+void InputDisplay();
+
+
+
+#endif /* INC_SEGMENT_H_ */
